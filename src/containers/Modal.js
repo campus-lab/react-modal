3ee26d30392ee2cb5c9bbb2be97a3fe4db780bc1
Alexandre Neves <alexandre-neves@telecom.pt>
2016-05-25 18:11:35 +0100

    fixed example

--- src/containers/Modal.js
+++ src/containers/Modal.js
@@ -2,11 +2,11 @@ import {connect} from 'react-redux';
 import {modalToggle} from '../actions';
 import _ from 'lodash';
 
-import modalWrapper from '../../../src/modalWrapper';
+import ModalWrapper from '../../../src/ModalWrapper';
 
 const mapStateToProps = (state, ownProp) => {
     if (state.modalToggle.id === ownProp.id) {
-        console.log('mapStateToProps', state, ownProp);
+        // console.log('mapStateToProps', state, ownProp);
         return {
             open: state.modalToggle.open
         };
@@ -15,7 +15,7 @@ const mapStateToProps = (state, ownProp) => {
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    console.log('mapDispatchToProps', ownProps);
+    // console.log('mapDispatchToProps', ownProps);
     return {
         onWindowClick: () => { dispatch(modalToggle()) }
     }
@@ -24,6 +24,6 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 const Modal = connect(
     mapStateToProps,
     mapDispatchToProps
-)(modalWrapper);
+)(ModalWrapper);
 
 export default Modal;
